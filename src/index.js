298196d823dbba57c3f567a2f7610ad3e253a33e
jayaimzzz <jameshorton@gmail.com>
2018-11-28 15:49:13 -0500

    removed basename from router.  concatnating path in the route

--- src/index.js
+++ src/index.js
@@ -13,11 +13,10 @@ const store = createStore(
     reducer, /* preloadedState, */
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
-  console.log(process.env.PUBLIC_URL)
 const routing = (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <Provider store={store}>
-      <Route render={(props)=><App pathname={props.location.pathname}/>}/>
+      <Route render={(props)=><App pathname={process.env.PUBLIC_URL + "/" + props.location.pathname}/>}/>
     </Provider>
   </BrowserRouter>
 );
